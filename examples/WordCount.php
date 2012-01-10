b70933824cafcf68c6ffabeae45a325d06ad647a
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-01-10 21:56:37 +0900

    Add: Example Map/Reduce procedure definition.

---
+++ examples/WordCount.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * WordCount example for MyMR.
+ *
+ * @author Yuya Takeyama
+ */
+use \MyMR\Base;
+
+class WordCount extends Base
+{
+    public function map($key, $value)
+    {
+        $words = preg_split('/\s+/u', $value['text']);
+        foreach ($words as $word) {
+            $this->emit($word, array('count' => 1));
+        }
+    }
+
+    public function reduce($key, $values)
+    {
+        $result = array('count' => 0);
+        foreach ($values as $value) {
+            $result['count'] += $value['count'];
+        }
+        return $result;
+    }
+}
