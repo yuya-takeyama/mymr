1b3121afee4d7a0ca84fe8666616939cd07caa76
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-01-11 11:39:26 +0900

    Add: Example database dump.

---
+++ examples/sql/wordcount_example_db.sql
@@ -0,0 +1,19 @@
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `texts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `text` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO `texts` VALUES (1,'to be or not to be'),(2,'to see is to believe'),(3,'art is long life is short');
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `word_counts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `key` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `count` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `key` (`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
