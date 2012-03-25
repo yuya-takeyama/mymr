c2cbaae227349f986b49ca062a1938f5b63b6db5
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-03-25 15:22:29 +0900

    Add Progress.

---
+++ src/MyMR/Progress.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author Yuya Takeyama
+ */
+namespace MyMR;
+
+use \Symfony\Component\Console\Output\OutputInterface;
+
+class Progress
+{
+    /**
+     * @var int
+     */
+    protected $max;
+
+    /**
+     * @var OutputInterface
+     */
+    protected $output;
+
+    /**
+     * Constructor.
+     *
+     * @param  int             $max    Max count of progression.
+     * @param  OutputInterface $output
+     */
+    public function __construct($max, OutputInterface $output)
+    {
+        $this->max = $max;
+        $this->output = $output;
+    }
+
+    public function setCurrentPosition($position)
+    {
+        $max = $this->max;
+        if ($position === 1) {
+            $this->output->writeln(sprintf('%d/%d (%d%%)', 0, $max, 0));
+        }
+        if ($position === $max) {
+            $this->output->writeln(sprintf('%d/%d (%d%%)', 1, $max, 100));
+        }
+    }
+}
--- tests/MyMR/Tests/ProgressTest.php
+++ tests/MyMR/Tests/ProgressTest.php
@@ -1,6 +1,40 @@
 <?php
 namespace MyMR\Tests;
 
+use \MyMR\Progress;
+
+use \SymfonyX\Component\Console\Output\SpyOutput;
+
 class ProgressTest extends \PHPUnit_Framework_Testcase
 {
+    /**
+     * @test
+     * @dataProvider provideInputsOfProgress
+     */
+    public function progress_should_work_correctly($max, $inputs, $expected)
+    {
+        $spy = new SpyOutput;
+        $progress = new Progress($max, $spy);
+        foreach ($inputs as $input) {
+            $progress->setCurrentPosition($input);
+        }
+        $this->assertEquals($expected, $spy->getMessage());
+    }
+
+    public function provideInputsOfProgress()
+    {
+        $data = array();
+
+        $data[] = array(
+            1,
+            array(1),
+            <<< __EXPECTED__
+0/1 (0%)
+1/1 (100%)
+
+__EXPECTED__
+        );
+
+        return $data;
+    }
 }
