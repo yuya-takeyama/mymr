770fb3aa2b367dc1c42bd49399db941dca9d8708
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-03-25 19:33:25 +0900

    Add option for builder script based procedure.

--- src/MyMR/Command/BuilderCommand.php
+++ src/MyMR/Command/BuilderCommand.php
@@ -26,12 +26,22 @@ class BuilderCommand extends Command
             ->setDescription('Executes Map/Reduce procedure from Builder interface')
             ->setDefinition(array(
                 new InputArgument('file', InputArgument::REQUIRED, 'File builds Map/Reduce with Builder interface')
-            ));
+            ))
+            ->addOption('input', 'i', InputOption::VALUE_OPTIONAL, 'URI of input table')
+            ->addOption('output', 'o', InputOption::VALUE_OPTIONAL, 'URI of output table');
     }
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $builder = require $input->getArgument('file');
+        $inputTable = $input->getOption('input');
+        if ($inputTable) {
+            $builder->setInputTable($inputTable);
+        }
+        $outputTable = $input->getOption('output');
+        if ($outputTable) {
+            $builder->setOutputTable($outputTable);
+        }
         $builder->getMapReduce()->execute($output);
     }
 }
